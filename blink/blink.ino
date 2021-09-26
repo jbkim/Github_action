9a3d1cc8f420dd3cc31dff4e7b4940c7f7d02eee
Jinbuhm Kim <jinbuhm.kim@gmail.com>
2021-09-26 15:07:52 +0900

    test no0error case

--- blink/blink.ino
+++ blink/blink.ino
@@ -25,8 +25,7 @@
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED, OUTPUT);  // for error test
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // the loop function runs over and over again forever
